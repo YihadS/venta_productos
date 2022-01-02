8246aacaa3bf4f8c818d72d2c57df96284d47895
Yihad Salek Villalba <72087335+YihadS@users.noreply.github.com>
2022-01-02 15:00:20 -0400

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,18 +1,17 @@
 <?php
 
-$sname= "127.0.0.1";
+$sname= "localhost";
 
-$unmae= "root";
+$uname= "root";
 
 $password = "";
 
 $db_name = "control_inventario";
 
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+$conn = mysqli_connect($sname, $uname, $password, $db_name);
 
 if (!$conn) {
 
     echo "La conexión ha fallado";
 
 }
-?>
